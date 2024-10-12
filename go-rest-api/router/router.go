f644952436ee93b8ae18f78f94440817c896417d
shinnosuke-suzuki <shinnosuke.suzuki@geniee.co.jp>
2024-10-12 13:57:10 +0900

    task関連のエンドポイント作成

--- go-rest-api/router/router.go
+++ go-rest-api/router/router.go
@@ -4,7 +4,9 @@ import (
 	"go-rest-api/controller"
 	"go-rest-api/repository"
 	"go-rest-api/usecase"
+	"os"
 
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 )
@@ -16,9 +18,24 @@ func NewRouter(db *gorm.DB) *echo.Echo {
 	uu := usecase.NewUserUsecase(ur)
 	uc := controller.NewUserController(uu)
 
+	tr := repository.NewTaskRepository(db)
+	tu := usecase.NewTaskUsecase(tr)
+	tc := controller.NewTaskController(tu)
+
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.LogOut)
+
+	t := e.Group("/tasks")
+	t.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey:  []byte(os.Getenv("SECRET")),
+		TokenLookup: "cookie:token",
+	}))
+	t.GET("", tc.GetAll)
+	t.GET("/:id", tc.GetByID)
+	t.POST("", tc.Create)
+	t.PUT("/:id", tc.Update)
+	t.DELETE("/:id", tc.Delete)
 
 	return e
 }
