bf8625d5b1f631805fdfa2de21ed5184ac58afb9
shinnosuke-suzuki <shinnosuke.suzuki@geniee.co.jp>
2024-10-12 18:28:46 +0900

    csrfエンドポイントとミドルウェアの作成

--- go-rest-api/router/router.go
+++ go-rest-api/router/router.go
@@ -5,15 +5,34 @@ import (
 	"go-rest-api/repository"
 	"go-rest-api/usecase"
 	"go-rest-api/validator"
+	"net/http"
 	"os"
 
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"gorm.io/gorm"
 )
 
 func NewRouter(db *gorm.DB) *echo.Echo {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000", os.Getenv("FE_URL")},
+		AllowHeaders: []string{echo.HeaderOrigin,
+			echo.HeaderContentType,
+			echo.HeaderAccept,
+			echo.HeaderAccessControlAllowHeaders,
+			echo.HeaderXCSRFToken},
+		AllowMethods:     []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+		AllowCredentials: true,
+	}))
+	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		CookiePath:     "/",
+		CookieDomain:   os.Getenv("API_DOMAIN"),
+		CookieHTTPOnly: true,
+		CookieSecure:   true,
+		CookieSameSite: http.SameSiteDefaultMode,
+	}))
 
 	ur := repository.NewUserRepository(db)
 	uv := validator.NewUserValidator()
@@ -28,6 +47,7 @@ func NewRouter(db *gorm.DB) *echo.Echo {
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.LogOut)
+	e.GET("/csrf", uc.CsrfToken)
 
 	t := e.Group("/tasks")
 	t.Use(echojwt.WithConfig(echojwt.Config{
